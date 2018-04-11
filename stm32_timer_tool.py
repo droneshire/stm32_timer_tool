fbd4096a163523096ad4d7d69b9395bb1a3607d5
Ross Yeager <rossyeager@users.noreply.github.com>
2018-04-11 10:44:01 -0700

    Update stm32_timer_tool.py

--- stm32_timer_tool.py
+++ stm32_timer_tool.py
@@ -15,15 +15,21 @@ if __name__ == '__main__':
     max_reload = 0
     best_prescalar = 0
     fclk = args.clock
+    div = 1
     for i in range(3):
-        fclk = fclk / (i**2 + 1)
+        clk_div = (i**2 + 1)
+        fclk = fclk / clk_div
         for prescalar in range(2**16 - 1):
             reload_val = int(float(fclk) / float(prescalar + 1) / args.target - 1)
-            if reload_val >= 2**16 or reload_val <=10:
+            if reload_val >= 2**16 or reload_val <= 0.0:
                 continue
             if reload_val > max_reload:
                 max_reload = reload_val
                 best_prescalar = prescalar
-                div = i + 1
+                div = clk_div
 
     print('CLKDIV: {} ARR: {} PRE: {}'.format(div, max_reload, best_prescalar))
+    actual =  args.clock / div / (best_prescalar + 1) / (max_reload  + 1)
+    print('Desired: {} Actual: {} Error: {}%'.format(args.target, actual, float(actual - args.target) * 100.0 / float(args.target)))
+    if actual != args.target:
+        print('WARNING: Can\'t achieve desired frequecy with current timer clock frequency!')
